cf30ffe3674aa604ca2f7c02679dd9591f9ede5e
Ivan Gureev <ig@hexn.io>
2025-04-26 01:53:01 +0300

    feature/from_str_parse: Add decimal_rc bench

--- benchmark/benches/conversions.rs
+++ benchmark/benches/conversions.rs
@@ -204,6 +204,43 @@ fn bench_conversions_rust_decimal(c: &mut Criterion) {
     group.finish();
 }
 
+#[cfg(feature = "comparison")]
+fn bench_conversions_decimal_rc(c: &mut Criterion) {
+    let mut group = c.benchmark_group("[decimal_rc]");
+
+    for precision in PRECISIONS {
+        let mut iter = RandomF64StringIterator::new(SEED, precision, -100_000.0..=100_000.0);
+        group.bench_function(
+            format!("parse from string {}_precision", precision).as_str(),
+            |b| {
+                b.iter_batched(
+                    || iter.next().unwrap(),
+                    |data| {
+                        let result = data.parse::<decimal_rs::Decimal>().unwrap();
+                        black_box(result)
+                    },
+                    BatchSize::SmallInput,
+                )
+            },
+        );
+    }
+
+    group.bench_function("parse entire file", |b| {
+        let data =
+            read_file_as_strings("test_data/prices.log").expect("Failed to read data from file");
+
+        b.iter(|| {
+            let results: Vec<decimal_rs::Decimal> = data
+                .iter()
+                .map(|line| line.parse::<decimal_rs::Decimal>().unwrap())
+                .collect();
+            black_box(results)
+        });
+    });
+
+    group.finish();
+}
+
 #[cfg(not(feature = "comparison"))]
 criterion_group!(benches, bench_conversions);
 
@@ -214,7 +251,8 @@ criterion_group!(
     bench_conversions_f64,
     bench_conversions_fastnum,
     bench_conversions_bigdecimal,
-    bench_conversions_rust_decimal
+    bench_conversions_rust_decimal,
+    bench_conversions_decimal_rc,
 );
 
 criterion_main!(benches);
